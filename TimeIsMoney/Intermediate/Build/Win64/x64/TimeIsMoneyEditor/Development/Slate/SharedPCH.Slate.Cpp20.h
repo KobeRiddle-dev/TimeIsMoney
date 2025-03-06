b43c822bd482f3ec08d3124b3b32747a2fb2fc4e
lordboompa <lordboompa@gmail.com>
2025-03-05 18:05:56 -0700

    Initial

--- TimeIsMoney/Intermediate/Build/Win64/x64/TimeIsMoneyEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "C:/Goode/4500/TimeIsMoney/TimeIsMoney/Intermediate/Build/Win64/x64/TimeIsMoneyEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
--- TimeIsMoney/Intermediate/Build/Win64/x64/TimeIsMoneyEditor/Development/UnrealEd/SharedPCH.UnrealEd.Project.ValApi.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
-#include "C:/Goode/4500/TimeIsMoney/TimeIsMoney/Intermediate/Build/Win64/x64/TimeIsMoneyEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Project.ValApi.Cpp20.h"
-#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
